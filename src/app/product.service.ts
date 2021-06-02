14e9fbf07282666e4913b4e19663e5f984049182
JonathanIntrica <jonathan@intrica.net>
2021-06-02 11:18:22 +0100

    Fixed Import Statement and AlbumUrl Location

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, Response} from '@angular/http';
 
 @Injectable()
 export class ProductService {
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) {  }
-
-  _albumUrl = '../assets/album.json';
-
   getAlbum(id: number) {
     return this._http.get(this._albumUrl);
   }
