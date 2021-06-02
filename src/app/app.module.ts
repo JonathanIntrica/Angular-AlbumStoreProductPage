7846f42bda2270611c9286c0cf03c8db8639ddce
JonathanIntrica <jonathan@intrica.net>
2021-06-02 13:04:30 +0100

    Route Setup
    Setup Routes in app module, added links to the product list, and changed the main app component to show the router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
@@ -10,8 +10,13 @@ import {ProductService} from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
+  const appRoutes: Routes = [
+    { path: '', redirectTo: 'products', pathMatch: 'full' },
+    { path: 'products', component: ProductListComponent },
+    { path: 'product/:id', component: ProductPageComponent }
+  ]
+
 @NgModule({
-  appRoutes: Routes;
   declarations: [
     AppComponent,
     ProductPageComponent,
@@ -22,6 +27,7 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
     HttpModule
   ],
   providers: [
