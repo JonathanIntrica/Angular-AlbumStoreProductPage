c7b5eb8a0ab67ae37c7a362a8ce90d17869e7db8
JonathanIntrica <jonathan@intrica.net>
2021-06-02 12:51:27 +0100

    Pushing to see Constant Declaration Example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
@@ -11,6 +11,7 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
+  appRoutes: Routes;
   declarations: [
     AppComponent,
     ProductPageComponent,
